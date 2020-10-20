c572602df7eaa3c3e4412adb1f91663da022b9b2
gj185048 <gj185048@ncr.com>
2020-10-20 19:29:12 +0530

    ignore

--- src/main/java/agilecraftDataSync/example/ExampleApplication.java
+++ src/main/java/agilecraftDataSync/example/ExampleApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+//hello
 public class ExampleApplication {
 
 	public static void main(String[] args) {
